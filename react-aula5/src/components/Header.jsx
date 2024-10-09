6a73a53e436f5d5b00a4d0214babbc70d756f735
Pedro Simão <pedrosimaocontato@gmail.com>
2024-10-09 12:53:43 -0300

    aula react prime react

--- react-aula5/src/components/Header.jsx
+++ react-aula5/src/components/Header.jsx
@@ -1,19 +1,43 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import styled from "styled-components"
+
+const Headercontainer = styled.header`
+    background-color: blueviolet;
+    padding: 0 60px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: Arial, Helvetica, sans-serif;
+    & h1 {
+        color: white;
+    }
+
+    & nav ul {
+        display: flex;
+        gap: 36px;
+        list-style: none;
+        & li a {
+            color: #ffffff70;
+            text-decoration: none;
+            font-size: 18px;
+            &:hover, &.active{
+                color: #ffffff;
+            }
+        }
+    }
+;`
 
 const Header = () => {
     return (
-        <>
-            <header>
+        <Headercontainer>
                 <h1>LOGO</h1>
                 <nav>
                     <ul>
-                        <li><Link to={"/"}>Home</Link></li>
-                        <li><Link to={"/produtos"}>Produtos</Link></li>
+                        <li><NavLink to={"/"}>Home</NavLink></li>
+                        <li><NavLink to={"/produtos"}>Produtos</NavLink></li>
                     </ul>
                 </nav>
-            </header>
-        
-        </>
+        </Headercontainer>
     );
 }
 
---
+++ react-aula6/src/App.jsx
@@ -0,0 +1,15 @@
+import "primereact/resources/themes/lara-light-blue/theme.css";
+import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+
+const App = () => {
+  
+  
+  return(
+      <>
+
+      </>
+    );
+}
+
+export default App;
