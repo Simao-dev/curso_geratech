5aeb40f762487d9300a304f75472f8c0887e4d37
Pedro Simão <pedrosimaocontato@gmail.com>
2024-10-09 22:09:01 -0300

    formulário react aula6

--- react-aula6/src/App.jsx
+++ react-aula6/src/App.jsx
@@ -1,13 +1,14 @@
 import "primereact/resources/themes/lara-light-blue/theme.css";
 import "primeflex/primeflex.css";
 import "primeicons/primeicons.css";
+import Paths from "./routes/Paths";
 
 const App = () => {
   
   
   return(
       <>
-
+        <Paths />
       </>
     );
 }
