b116ec554ae381624d37df3b00ec2f19d354b4af
Pedro Simão <pedrosimaocontato@gmail.com>
2024-10-18 22:14:25 -0300

    aula 1 backand

---
+++ back-and/aula-nodejs/routes/produtos.js
@@ -0,0 +1,33 @@
+function listarProdutos() {
+    
+    
+    const produtos = [
+        {
+            id: 1,
+            nome:'Mouse USB',
+            valor:29.90
+        },
+        {
+            id: 2,
+            nome:'Teclado USB',
+            valor:29.90
+        }
+    ];
+    
+    return produtos;
+
+}
+
+function addProdutos() {
+
+}
+
+function editarProdutos() {
+    
+}
+
+function excluirProdutos() {
+    
+}
+
+module.exports = listarProdutos;
---
+++ back-and/aula-nodejs/server.js
@@ -0,0 +1,56 @@
+const { createServer } = require('node:http')
+const listarProdutos = require('./routes/produtos')
+const host = 'localhost'
+const port = 3000;
+//http://localhost:3000
+
+
+const app = createServer((resquest, response) => {   
+
+    const { url, method } = resquest;
+
+    console.log("URL - ", url);
+    console.log("Método/Verbo - ", method);
+
+    if (url === '/') {
+        response.writeHead(200, { 'Content-type': 'text/plain' });
+    return response.end("Olá nodeJS opa ");
+
+    }
+
+    if(url ==='/produtos') {
+        
+        const dados = listarProdutos();
+        
+        response.writeHead(200, { 'Content-type': 'application/json' });
+        return response.end(JSON.stringify(dados));
+    }
+
+    if(url ==='/produtos/adicionar') {    
+        
+        produtos.push({
+            id: 3,
+            nome:!'monitor 34p',
+            valor: 3330.00
+        })
+        
+        response.writeHead(200, { 'Content-type': 'text/plain' });
+        return response.end("produto adicionado com sucesso!");
+    }
+    if(url ==='/produtos/remover') {    
+        
+        produtos.push()
+        
+        response.writeHead(200, { 'Content-type': 'text/plain' });
+        return response.end("produto removido com sucesso!");
+    }
+    
+    response.writeHead(404, { 'Content-type': 'text/plain' });
+    return response.end("pagina não encontrada - Not Found ");
+
+
+});
+
+app.listen(port, host, () => {
+    console.log(`Servidor NodeJS executando http://${host}:${port}`)
+})
